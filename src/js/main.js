b33597d5fa7d3dde3deecfffbb78cae52cc18e11
Vladik <kamlasa@ukr.net>
2017-12-16 00:41:35 +0300

    ошибка

--- src/js/main.js
+++ src/js/main.js
@@ -15,13 +15,9 @@ close.addEventListener('click', function (evt) {
 
 $(document).ready(function(){
     $('.slider').slick({
-
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
 });
-});
-
-$('.multiple-items').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
 });
 
