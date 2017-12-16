586d74bab3be4c3ad014657ac3de7991060471c1
Vlad <greensmoke71@gmail.com>
2017-12-16 19:46:07 +0200

    issue plugin

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,11 @@ const sassGlob = require('gulp-sass-glob');
 const cleanCSS = require('gulp-cleancss');
 const autoprefixer = require('gulp-autoprefixer');
 const rigger = require('gulp-rigger');
+const tinypng = require('gulp-tinypng-compress');
+const svgSprite = require('gulp-svg-sprite');
+const svgmin = require('gulp-svgmin');
+const cheerio = require('gulp-cheerio');
+const replace = require('gulp-replace');
 
 
 const concat = require('gulp-concat');
@@ -20,6 +25,43 @@ const paths =  {
   build: './build/'           // paths.build
 };
 
+var config = {
+    mode: {
+        symbol: {
+            sprite: "sprite.svg",
+            render: {
+                scss: {
+                    dest: '../../../scss/_sprite.scss'
+                }
+            }
+        }
+    }
+};
+
+function svgSpriteBuild() {
+    return gulp.src( paths.src +'img/*.svg')
+    // минифицируем svg
+        .pipe(svgmin({
+            js2svg: {
+                pretty: true
+            }
+        }))
+        .pipe(cheerio({
+            run: function($) {
+                $('[fill]').removeAttr('fill');
+                $('[stroke]').removeAttr('stroke');
+                $('[style]').removeAttr('style');
+            },
+            parserOptions: {
+                xmlMode: true
+            }
+        }))
+        .pipe(gulp-replace('&gt;', '>'))
+        // build svg sprite
+        .pipe(svgSprite(config))
+        .pipe(gulp.dest( paths.build +'img/'));
+};
+
 function styles() {
   return gulp.src(paths.src + 'sass/main.scss')
     .pipe(sassGlob())
@@ -30,6 +72,7 @@ function styles() {
     .pipe(rename({ suffix: ".min" }))
     .pipe(gulp.dest(paths.build + 'css/'))
 }
+
 
 function scripts() {
   return gulp.src(paths.src + 'js/*.js')
@@ -45,7 +88,11 @@ function htmls() {
 
 
 function img() {
-    return gulp.src(paths.src + 'img/**/*')
+    return gulp.src(paths.src + 'img/**/*.{png,jpg,jpeg}')
+        .pipe(tinypng({
+            key: 'tNK8DHr7L24S5Ygz3BsfqDqn0w0yJzpn',
+            log: true
+        }))
         .pipe(gulp.dest(paths.build + "img"));
 }
 
@@ -62,6 +109,7 @@ function watch() {
   gulp.watch(paths.src + 'sass/**/*.scss', styles);
   gulp.watch(paths.src + 'js/*.js', scripts);
   gulp.watch(paths.src + '*.html', htmls);
+  gulp.watch(paths.src + 'src/img/*', img);
 }
 
 function serve() {
@@ -80,6 +128,7 @@ exports.clean = clean;
 exports.watch = watch;
 exports.img = img;
 exports.fonts = fonts;
+exports.svgSpriteBuild = svgSpriteBuild;
 
 gulp.task('build', gulp.series(
   clean,
@@ -87,13 +136,14 @@ gulp.task('build', gulp.series(
   scripts,
   htmls,
   img,
-  fonts
+  fonts,
+  svgSpriteBuild
   // gulp.parallel(styles, scripts, htmls)
 ));
 
 gulp.task('default', gulp.series(
   clean,
-  gulp.parallel(styles, scripts, htmls, img, fonts),
+  gulp.parallel(styles, scripts, htmls, img, fonts, svgSpriteBuild),
   gulp.parallel(watch, serve)
 ));
 
