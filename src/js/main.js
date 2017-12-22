4bec7b6274cc524de544da75d6673280a0b16598
Vlad <greensmoke71@gmail.com>
2017-12-22 23:44:18 +0200

    footer

--- src/js/main.js
+++ src/js/main.js
@@ -25,3 +25,58 @@ $(function() {
     });
 });
 
+
+
+
+
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17,
+        center: new google.maps.LatLng(59.896226, 30.424275),
+        mapTypeId: 'roadmap'
+    });
+
+
+    var icons = {
+        logo: {
+            icon: {
+                url: "../img/symbol/sprite.svg#map-marker",
+                size: new google.maps.Size(50, 60),
+                scaledsize: new google.maps.Size(50, 60)
+            }
+        }
+    };
+
+    var features = [
+        {
+            position: new google.maps.LatLng(59.896248, 30.424275),
+            type: 'logo'
+        } ,
+
+        {
+            position: new google.maps.LatLng(59.895356, 30.423146),
+            type: 'logo'
+        },
+
+        {
+            position: new google.maps.LatLng(59.894529, 30.417746),
+            type: 'logo'
+        },
+
+        {
+            position: new google.maps.LatLng(59.898440, 30.428543),
+            type: 'logo'
+        }
+    ];
+
+    // Create markers.
+    features.forEach(function(feature) {
+        var marker = new google.maps.Marker({
+            position: feature.position,
+            icon: icons[feature.type].icon,
+            map: map
+        });
+    });
+}
+
